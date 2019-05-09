3f151f1c8897e03e8663f72345130eb49703cd0a
Cara Yung <yungcara@gmail.com>
2019-05-09 19:50:36 -0400

    CardInfo classes

--- app/src/main/java/com/example/rowcounter/CardTypes/CardInfo.kt
+++ app/src/main/java/com/example/rowcounter/CardTypes/CardInfo.kt
@@ -1,17 +1,19 @@
 package com.example.rowcounter.CardTypes
 
-import android.content.res.Resources
-import com.example.rowcounter.R
+open class CardInfo(cardType: Int, cardName: String) {
+    val cardType: Int = cardType
+    var cardName: String = cardName
 
-class CardInfo(cardType: Int, cardName: String) {
-    val cardType: Int
-    var cardName: String
+}
 
-    init {
-        this.cardType = cardType
-        this.cardName = cardName
+open class CounterCardInfo(cardType: Int, cardName: String, displayValue: Int) : CardInfo(cardType, cardName) {
+    var displayValue: Int = displayValue
 
+}
 
-    }
+class SecondaryCounterCardInfo(cardType: Int, cardName: String, displayValue: Int, isLinked: Boolean, repeatLimit: Int)
+    : CounterCardInfo(cardType, cardName, displayValue) {
+    var isLinked : Boolean = isLinked
+    var repeatLimit : Int = repeatLimit
 
 }
